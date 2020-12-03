9d157ba0c1c69a54a99969277a343632985957ca
Manas Ranjan Chhotray <manas.c12@gmail.com>
2020-12-03 19:37:10 +0100

    Add files via upload

--- train_nn_model.py
+++ train_nn_model.py
@@ -79,7 +79,7 @@ headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
 headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
-headModel = Dense(2, activation="softmax")(headModel)
+headModel = Dense(2, activation="sigmoid")(headModel)
 
 # place the head FC model on top of the base model (this will become
 # the actual model we will train)
